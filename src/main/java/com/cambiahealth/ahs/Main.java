804aa6e9ebd316b4fcf4c68776e256bba1016960
Brenton Pyl (r627021) <brenton.pyl@cambiahealth.com>
2016-02-18 09:39:46 -0800

    Adding main repo

---
+++ src/main/java/com/cambiahealth/ahs/Main.java
@@ -0,0 +1,33 @@
+package com.cambiahealth.ahs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Get next Ctg
+        // Loop: Get next meme
+
+        // Process Address (don't forget zip codes)
+        // If no remote addresses present, stop processing
+
+        // Merge Eligibility with CSPI History then ClaimConfiguration
+        // If no data present, stop processing
+        // Store 4 "most recent" columns during this walk.
+
+        // Process COB
+
+        // Reduce Member History
+
+        // Final Merging
+        // Store to processed rows buffer
+
+        // End loop condition (no more meme's in CTG set)
+
+        // If more than one processed row pending, then
+        // Post process COB
+
+        // Flush processed rows to flat file.
+
+        // Loop Ctg
+        System.out.println("I'm a java JAR!");
+    }
+}
