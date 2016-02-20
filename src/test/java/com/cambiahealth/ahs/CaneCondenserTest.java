a74b3025b88191bf3d6e863b754bc821886d9f39
Danny <Daniel.Goldstein@cambiahealth.com>
2016-02-19 16:16:55 -0800

    test cane condenser

---
+++ src/test/java/com/cambiahealth/ahs/CaneCondenserTest.java
@@ -0,0 +1,20 @@
+package com.cambiahealth.ahs;
+
+import com.cambiahealth.ahs.timeline.Timeline;
+import com.cambiahealth.ahs.timeline.TimelineContext;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by r628601 on 2/19/2016.
+ */
+public class CaneCondenserTest {
+
+
+    @Test
+    public void testOutputRowTo2A(){
+        Map<TimelineContext,Timeline> = new HashMap<TimelineContext>()
+    }
+}
